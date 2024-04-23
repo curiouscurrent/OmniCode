385c962e8a48610b57fcda68d4a573119e210d32
Anusha K <95173109+curiouscurrent@users.noreply.github.com>
2024-04-23 15:03:36 +0530

    Update app.py
    changed from streamlit to gradio

--- app.py
+++ app.py
@@ -1,43 +1,40 @@
-import streamlit as st
 import requests
 import json
+import gradio as gr
 
-url = "http://localhost:11434/api/generate"
+url="http://localhost:11434/api/generate"
 
-headers = {
-    'Content-Type': 'application/json'
+headers={
+
+    'Content-Type':'application/json'
 }
 
-history = []
+history=[]
 
 def generate_response(prompt):
     history.append(prompt)
-    final_prompt = "\n".join(history)
+    final_prompt="\n".join(history)
 
-    data = {
-        "model": "omnicode",
-        "prompt": final_prompt,
-        "stream": False
+    data={
+        "model":"omnicode",
+        "prompt":final_prompt,
+        "stream":False
     }
 
-    response = requests.post(url, headers=headers, data=json.dumps(data))
+    response=requests.post(url,headers=headers,data=json.dumps(data))
 
-    if response.status_code == 200:
-        response = response.text
-        data = json.loads(response)
-        actual_response = data['response']
+    if response.status_code==200:
+        response=response.text
+        data=json.loads(response)
+        actual_response=data['response']
         return actual_response
     else:
-        st.error("Error occurred: {}".format(response.text))
+        print("error:",response.text)
 
-def main():
-    st.title("Omnicode Chat")
-    prompt = st.text_area("Enter your Prompt", height=200)
-    if st.button("Generate Response"):
-        response = generate_response(prompt)
-        if response:
-            st.write("Response:")
-            st.write(response)
 
-if __name__ == "__main__":
-    main()
+interface=gr.Interface(
+    fn=generate_response,
+    inputs=gr.Textbox(lines=4,placeholder="Enter your Prompt"),
+    outputs="text"
+)
+interface.launch()
